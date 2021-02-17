7f9fd4b34c9ce05808ff2df148b456a429a91bf4
Lucas Fell <lucasafell@gmail.com>
2021-02-16 22:41:35 -0300

    changed folder structure, deleted backend files

--- imobiliaria/src/main/java/com/unipampa/crud/interfaces/service/IPropertyService.java
+++
@@ -1,9 +0,0 @@
-package com.unipampa.crud.interfaces.service;
-
-import com.unipampa.crud.dto.PropertyDTO;
-import com.unipampa.crud.model.Property;
-
-public interface IPropertyService {
-	
-	void saveProperty(Property property);
-}
--- imobiliaria/src/main/java/com/unipampa/crud/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unipampa.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.unipampa.crud.model.Property;
-
-public interface PropertyRepository extends JpaRepository<Property, Long>{
-
-}
