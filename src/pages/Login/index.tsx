d61b50a9732fe175ddf96def303714dfd3d32107
Lucas Fell <lucasafell@gmail.com>
2021-02-26 16:59:41 -0300

    styling

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,45 +5,43 @@ import { Input } from '../../components/Input';
 import { Label } from '../../components/Label';
 
 export default function Login () {
-    return (
-        <div className="flex w-screen h-screen">
-            <div className="flex-auto h-full bg-red-200">
-                <div className="flex-auto m-16">
-                    <h1 className="text-5xl mb-8 text-left font-bold text-red-700">
-                        <Link to="/">Imobiliária</Link>
-                    </h1>
-                    <h1 className="text-2xl text-left font-bold text-red-700">
-                        Descubra imóveis para compra e aluguel na sua área
-                    </h1>
-                </div>
-            </div>
-            <div className="flex-none w-3/4 h-full">
-                <div className="flex flex-col w-auto h-full">
-                    <div className="flex w-full justify-end p-8">
-                        <p>Ainda não é membro?&nbsp;</p>
-                        <Link className="text-red-700" to="/signup/new">Criar conta</Link>
-                    </div>
-                    <div className="flex justify-center items-center w-full h-full">
-                        <div className="flex flex-col px-48 w-3/5">
-                            <h1 className="text-3xl font-bold mb-8">Acesse sua conta</h1>
-                            <form action="" method="post">
-                                <fieldset>
-                                    <Label for="userEmail">Endereço de Email</Label>
-                                    <Input name="userEmail" type="email"/>
-                                </fieldset>
-                                <fieldset className="my-6">
-                                    <div className="flex justify-between">
-                                        <Label for="userPassword">Senha</Label>
-                                        <Link className="text-red-700" to="/passwords_reset/new">Esqueceu a senha?</Link>
-                                    </div>
-                                    <Input name="userPassword" type="password"/>
-                                </fieldset>
-                                <Button type="submit">Enviar</Button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex w-auto h-screen">
+      <div className="flex-initial h-full bg-red-200">
+        <div className="flex-auto m-16">
+          <h1 className="text-5xl mb-8 text-left font-bold text-red-700">
+              <Link to="/">Imobiliária</Link>
+          </h1>
+          <h1 className="text-2xl text-left font-bold text-red-700">
+              Descubra imóveis para compra e aluguel na sua área
+        </h1>
         </div>
-    )
+      </div>
+      <div className="flex flex-col w-11/12 h-full justify-items-stretch">
+        <div className="flex justify-end p-8">
+          <p>Ainda não é membro?&nbsp;</p>
+          <Link className="text-red-700" to="/signup/new">Criar conta</Link>
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <div className="flex flex-col w-2/5">
+            <h1 className="text-3xl font-bold mb-8">Acesse sua conta</h1>
+            <form action="" method="post">
+                <fieldset>
+                    <Label for="userEmail">Endereço de Email</Label>
+                    <Input name="userEmail" type="email"/>
+                </fieldset>
+                <fieldset className="my-6">
+                    <div className="flex justify-between">
+                        <Label for="userPassword">Senha</Label>
+                        <Link className="text-red-700" to="/passwords_reset/new">Esqueceu a senha?</Link>
+                    </div>
+                    <Input name="userPassword" type="password"/>
+                </fieldset>
+                <Button type="submit">Enviar</Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -44,8 +44,8 @@ export default function Signup () {
   }
 
   return (
-    <div className="flex w-screen h-screen">
-      <div className="flex-auto h-full bg-blue-200">
+    <div className="flex w-auto h-screen">
+      <div className="flex-initial h-full bg-blue-200 ">
         <div className="flex-auto m-16">
           <h1 className="text-5xl mb-8 text-left font-bold text-blue-700">
             <Link to="/">Imobiliária</Link>
@@ -55,93 +55,95 @@ export default function Signup () {
           </h1>
         </div>
       </div>
-      <div className="flex-none w-3/4 h-full">
-        <div className="flex flex-col w-auto h-full">
-          <div className="flex w-full justify-end p-8">
-            <p>Já é membro?&nbsp;</p>
-            <Link className="text-blue-700" to="/session/new">Acessar conta</Link>
-          </div>
-          <div className="flex justify-center items-center w-full h-full">
-            <div className="flex flex-col px-48 w-3/5">
-              <h1 className="text-3xl font-bold mb-8">Crie sua conta</h1>
-              <form onSubmit={handleSubmit}>
-                <fieldset>
-                  <Label for="userName">Nome</Label>
-                  <Input
-                    value={userName}
-                    onChange={e => setUserName(e.target.value)}
-                    color="blue"
-                    name="userName"
-                    type="text"
-                  />
-                </fieldset>
-                <fieldset className="my-4">
-                  <Label for="userEmail">Endereço de Email</Label>
-                  <Input
-                    value={userEmail}
-                    onChange={e => setUserEmail(e.target.value)}
-                    color="blue"
-                    name="userEmail"
-                    type="email"
-                  />
-                </fieldset>
-                <fieldset className="my-4">
-                  <div className="flex justify-between gap-5">
-                    <div>
-                      <Label for="userCpf">CPF</Label>
-                      <Input
-                        value={userCpf}
-                        onChange={e => setUserCpf(e.target.value)}
-                        color="blue"
-                        name="userCpf"
-                        type="text"
-                      />
-                    </div>
-                    <div>
-                      <Label for="userPhone">Telefone</Label>
-                      <Input
-                        value={userPhone}
-                        onChange={e => setUserPhone(e.target.value)}
-                        color="blue"
-                        name="userPhone"
-                        type="text"
-                      />
-                    </div>
+      <div className="flex flex-col w-11/12 h-full justify-items-stretch">
+        <div className="flex justify-end p-8">
+          <p>Já é membro?&nbsp;</p>
+          <Link className="text-blue-700" to="/session/new">Acessar conta</Link>
+        </div>
+        <div className="flex justify-center items-center h-full">
+          <div className="flex flex-col w-2/5">
+            <h1 className="text-3xl font-bold mb-8">Crie sua conta</h1>
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <Label for="userName">Nome</Label>
+                <Input
+                  value={userName}
+                  onChange={e => setUserName(e.target.value)}
+                  color="blue"
+                  name="userName"
+                  type="text"
+                />
+              </fieldset>
+              <fieldset className="my-4">
+                <Label for="userEmail">Endereço de Email</Label>
+                <Input
+                  value={userEmail}
+                  onChange={e => setUserEmail(e.target.value)}
+                  color="blue"
+                  name="userEmail"
+                  type="email"
+                />
+              </fieldset>
+              <fieldset className="my-4">
+                <div className="flex justify-between gap-6">
+                  <div className="w-1/2">
+                    <Label for="userCpf">CPF</Label>
+                    <Input
+                      value={userCpf}
+                      onChange={e => setUserCpf(e.target.value)}
+                      color="blue"
+                      name="userCpf"
+                      type="text"
+                    />
                   </div>
-                </fieldset>
-                <fieldset className="my-4">
-                  <Label for="userAddress">Endereço</Label>
-                  <Input
-                    value={userAddress}
-                    onChange={e => setUserAddress(e.target.value)}
-                    color="blue"
-                    name="userAddress"
-                    type="text"
+                  <div className="w-1/2">
+                    <Label for="userPhone">Telefone</Label>
+                    <Input
+                      value={userPhone}
+                      onChange={e => setUserPhone(e.target.value)}
+                      color="blue"
+                      name="userPhone"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset className="my-4">
+                <Label for="userAddress">Endereço</Label>
+                <Input
+                  value={userAddress}
+                  onChange={e => setUserAddress(e.target.value)}
+                  color="blue"
+                  name="userAddress"
+                  type="text"
+                />
+              </fieldset>
+              <fieldset className="my-4">
+                <div className="flex justify-between gap-6">
+                  <div className="w-1/2">
+                    <Label for="userPassword">Senha</Label>
+                    <Input
+                      value={userPassword}
+                      onChange={e => setUserPassword(e.target.value)}
+                      color="blue"
+                      name="userPassword"
+                      type="password"
+                    />
+                  </div>
+                  <div className="w-1/2">
+                    <Label for="userPasswordRepeat">Repita a senha</Label>
+                    <Input
+                      value={userPasswordRepeat}
+                      onChange={e => setUserPasswordRepeat(e.target.value)}
+                      color="blue"
+                      name="userPasswordRepeat"
+                      type="password"
                   />
-                </fieldset>
-                <fieldset className="my-4">
-                  <Label for="userPassword">Senha</Label>
-                  <Input
-                    value={userPassword}
-                    onChange={e => setUserPassword(e.target.value)}
-                    color="blue"
-                    name="userPassword"
-                    type="password"
-                  />
-                </fieldset>
-                <fieldset className="my-4">
-                  <Label for="userPasswordRepeat">Repita a senha</Label>
-                  <Input
-                    value={userPasswordRepeat}
-                    onChange={e => setUserPasswordRepeat(e.target.value)}
-                    color="blue"
-                    name="userPasswordRepeat"
-                    type="password"
-                  />
-                </fieldset>
-                <Button color="blue" type="submit">Enviar</Button>
-              </form>
-            </div>
+                  </div>
+                </div>
+              </fieldset>
+              <Button color="blue" type="submit">Enviar</Button>
+            </form>
           </div>
         </div>
       </div>
