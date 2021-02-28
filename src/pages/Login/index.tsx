252625c2680ef8bb2f7e0932c9046638d028b9f7
Lucas Fell <lucasafell@gmail.com>
2021-02-28 15:56:21 -0300

    mock login until authentication service is ready

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Label } from '../../components/Label';
+import { apiCrud } from '../../services/api';
 
 export default function Login () {
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+
+  const history = useHistory()
+
+  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    try {
+        // const response = await apiAuth.post('/session', { userEmail, userPassword })
+        const response = await apiCrud.get('/user/customer/find/1')
+
+        localStorage.setItem('userId', response.data.id)
+        localStorage.setItem('userName', response.data.name)
+        localStorage.setItem('userEmail', response.data.email)
+        localStorage.setItem('userCpf', response.data.cpf)
+        localStorage.setItem('userPhone', response.data.phone)
+        localStorage.setItem('userAddress', response.data.address)
+
+        history.push('/')
+    } catch (err) {
+        alert('Erro ao logar, tente novamente.')
+    }
+}
+
   return (
     <div className="flex w-auto h-screen">
       <div className="flex-initial h-full bg-red-200">
@@ -25,17 +51,27 @@ export default function Login () {
         <div className="flex justify-center items-center h-full">
           <div className="flex flex-col w-2/5">
             <h1 className="text-3xl font-bold mb-8">Acesse sua conta</h1>
-            <form action="" method="post">
+            <form onSubmit={handleLogin}>
                 <fieldset>
                     <Label for="userEmail">Endereço de Email</Label>
-                    <Input name="userEmail" type="email"/>
+                    <Input
+                      name="userEmail"
+                      type="email"
+                      value={userEmail}
+                      onChange={e => setUserEmail(e.target.value)}
+                    />
                 </fieldset>
                 <fieldset className="my-6">
                     <div className="flex justify-between">
                         <Label for="userPassword">Senha</Label>
                         <Link className="text-red-700" to="/passwords_reset/new">Esqueceu a senha?</Link>
                     </div>
-                    <Input name="userPassword" type="password"/>
+                    <Input
+                      name="userPassword"
+                      type="password"
+                      value={userPassword}
+                      onChange={e => setUserPassword(e.target.value)}
+                    />
                 </fieldset>
                 <Button type="submit">Enviar</Button>
             </form>
