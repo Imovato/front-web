7b1d9e0773de4240b52cb6ee7f2ffef0417a5f70
Lucas Fell <47724710+fell-lucas@users.noreply.github.com>
2021-03-21 14:16:48 -0300

    input text color

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,7 +11,8 @@ export function Input({ type, name, color, className, ...rest }: InputProps) {
     <input
       {...rest}
       className={`focus:ring focus:ring-${color ?? "red"}-200 w-full h-10
-        px-3 rounded-lg bg-gray-200 mt-2 dark:bg-gray-600 ${className}`}
+        dark:text-white px-3 rounded-lg bg-gray-200 mt-2 dark:bg-gray-600
+        ${className}`}
       type={type}
       name={name}
       id={name}/>
