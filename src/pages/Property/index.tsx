6e5ec677be783dcbc2d13fd60da32185c96af8de
Igor Dalepiane da Costa <igordalepiane@gmail.com>
2021-04-20 17:41:17 -0300

    Virtual visit

--- src/pages/Property/index.tsx
+++ src/pages/Property/index.tsx
@@ -11,6 +11,13 @@ import '@brainhubeu/react-carousel/lib/style.css';
 import ContactUsSvg from '../../assets/phone-call.svg'
 import { TimedDialog } from "../../components/TimedDialog";
 import { PropertyData } from "../../components/PropertyData";
+import quarto from '../../assets/quarto.jpg';
+import quartoNet from '../../assets/quartoNet.jpg';
+import quarto2 from '../../assets/quarto2.jpg';
+import sala from '../../assets/sala.jpg';
+import net1 from '../../assets/net1.jpg';
+import { Pannellum} from "pannellum-react";
+
 interface Property {
   id: string;
   name: string;
@@ -42,6 +49,7 @@ interface CustomerData {
 }
 
 function Property() {
+  // const photoSphere = require('photo-sphere-viewer');
   const [property, setProperty] = useState<Property>();
   const [acquisition, setAcquisition] = useState(false);
   const [stateImages, setStateImages] = useState<string[]>([])
@@ -50,14 +58,16 @@ function Property() {
   const [msgActions, setMsgActions] = useState<string[]>([])
   const [msgStart, setMsgStart] = useState(false)
   const [dialogStyle, setDialogStyle] = useState('success')
+  const [imagesPan, setImagesPan] = useState([''])
+  const [imagePan, setImagePan] = useState(0)
+  const [vaov, setVaov] = useState(65)
   const msgTimeout = 2500
 
   let params = useParams<RouteParams>();
 
   const history = useHistory()
-
   useEffect(() => {
-    
+    setImagesPan([quarto,sala,quarto2, net1,quartoNet])
     apiProperty
       .get("property/find/".concat(params.id), {})
       .then((response) => {
@@ -85,6 +95,19 @@ function Property() {
     phone: "",
   } as CustomerData);
 
+  function changePan(){
+    if(imagePan==imagesPan.length-1){
+      setImagePan(0)
+      setVaov(65)
+    }else{
+      setImagePan(imagePan+1)
+    }
+    if(imagePan==2 || imagePan==3){
+      setVaov(180)
+    }else{
+      setVaov(65)
+    }
+  }
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     try {
@@ -143,6 +166,7 @@ function Property() {
       <div className="pb-10 max-w-7xl m-auto h-screen">
         <div className="flex flex-col gap-12 items-center">
           <Navbar></Navbar>
+          
           <section
             className="flex flex-col items-center max-h-192 gap-6 w-full
             max-w-6xl overflow-y-auto scrollbar-thumb-rounded-full scrollbar-thin
@@ -202,17 +226,27 @@ function Property() {
                     <p>Já comprado</p>
                   </div>
                 )}
-                
+                <a href="#visitaVirtual" className="grid place-items-center bg-blue-400 h-12 text-lg text-white">
+                  <button  
+                    onClick={function(e) {
+                      changePan(); //can pass arguments this.btnTapped(foo, bar);          
+                    }}>
+                    <p>Visita Virtual</p>
+                  </button>
+                </a>
               </div>
             </div>
+            
+            
+                  
             <div className="flex max-w-5xl w-full justify-around shadow-md
               rounded-xl bg-white dark:text-white dark:bg-gray-800 p-4 gap-16"
             >
-              <PropertyData name="Localização" value={property?.adress}/>
+              <PropertyData name="Localização" value={property?.adress }/>
               <PropertyData name="Bairro" value={property?.neighborhood}/>
-              <PropertyData name="Número" value={property?.number}/>
-              <PropertyData name="Bloco" value={property?.block}/>
-              <PropertyData name="Quartos" value={property?.rooms}/>
+              <PropertyData name="Número" value={property?.number ? property?.number : "N/A"}/>
+              <PropertyData name="Bloco" value={property?.block ? property?.block : "N/A"}/>
+              <PropertyData name="Quartos" value={property?.rooms ? property?.rooms : "N/A"}/>
             </div>
             <div className="flex max-w-3xl w-full shadow-md rounded-xl
               justify-around items-center gap-12 bg-white dark:bg-gray-800
@@ -288,6 +322,32 @@ function Property() {
               </div>
               <div className="flex flex-1 p-8">
                 <img src={ContactUsSvg} alt="Entre em contato"/>
+              </div>
+            </div>
+            <div className="grid place-items-center gap-5 w-full h-full p-5 shadow-md
+              rounded-xl bg-white dark:text-white dark:bg-gray-800">
+              <p className="text-xl">Visita Virtual</p>
+              <button className="grid place-items-center bg-green-400 h-12 text-lg text-white p-3"
+                onClick={function(e) {
+                  changePan(); //can pass arguments this.btnTapped(foo, bar);          
+                }}>
+                <p>Proxima imagem</p>
+              </button>
+              <div id="visitaVirtual" className="grid place-items-center w-full h-full">
+                <Pannellum
+                width="90%"
+                height="500px"
+                image={imagesPan[imagePan]}
+                pitch={10}
+                vaov={vaov}
+                yaw={180}
+                hfov={110}
+                autoLoad
+                onLoad={() => {
+                    console.log("panorama loaded");
+                }}
+              >
+                </Pannellum>
               </div>
             </div>
           </section>
