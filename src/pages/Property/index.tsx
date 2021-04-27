8056ee2e9b610c43a11980624c99f9c729f016d1
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-04-27 17:24:18 -0300

    toast

--- src/pages/Property/index.tsx
+++ src/pages/Property/index.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useHistory} from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { Button } from "../../components/Button";
 import Chat from "../../components/Chat";
 import { Label } from "../../components/Label";
 import Navbar from "../../components/Navbar";
-import { apiAcquisition, apiContact, apiProperty, apiRent  } from "../../services/api";
+import { apiAcquisition, apiContact, apiProperty, apiRent } from "../../services/api";
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 
 import ContactUsSvg from '../../assets/phone-call.svg'
 import { PropertyData } from "../../components/PropertyData";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 interface Property {
   id: string;
   name: string;
@@ -46,10 +46,6 @@ function Property() {
   const [acquisition, setAcquisition] = useState(false);
   const [stateImages, setStateImages] = useState<string[]>([])
   const [carousel, setCarousel] = useState(0)
-  const [msg, setMsg] = useState<string[]>([])
-  const [msgActions, setMsgActions] = useState<string[]>([])
-  const [msgStart, setMsgStart] = useState(false)
-  const [dialogStyle, setDialogStyle] = useState('success')
   const msgTimeout = 2500
 
   let params = useParams<RouteParams>();
@@ -57,7 +53,7 @@ function Property() {
   const history = useHistory()
 
   useEffect(() => {
-    
+
     apiProperty
       .get("property/find/".concat(params.id), {})
       .then((response) => {
@@ -69,14 +65,14 @@ function Property() {
         setStateImages(images)
       });
     apiAcquisition.get(`/property/find/${params.id}`, {}).then((response) => {
-        if(response.data!=""){
-          setAcquisition(true);
-        }
-      });
+      if (response.data != "") {
+        setAcquisition(true);
+      }
+    });
     setData({
       ...data, message: `Olá, tenho interesse neste imóvel: ${property?.name},
         ${property?.adress}. Gostaria de marcar uma visita presencial, aguardo contato.`
-      })
+    })
   }, [params.id]);
 
   const [data, setData] = useState({
@@ -89,47 +85,39 @@ function Property() {
     e.preventDefault();
     try {
       await apiContact.post("/contact", data);
-      setDialogStyle('success')
-      setMsg(['Mensagem enviada, entraremos em contato em breve.'])
-      setMsgStart(true)
-      setTimeout(() => {
-        setMsg([])
-      }, msgTimeout)
+      toast('Mensagem enviada, entraremos em contato em breve.', { autoClose: msgTimeout, type: 'success' })
     } catch (error) {
-      alert("Algo deu errado, tente novamente.");
+      toast('Algo deu errado, tente novamente.', { autoClose: msgTimeout, type: 'error' })
     }
   }
 
   function handleBuy() {
     try {
-      apiAcquisition.post("/save",{
+      apiAcquisition.post("/save", {
         data: new Date(),
-        value:property?.price,
-        amount:property?.amount,
-        idProperty:property?.id,
-        idUser:localStorage.getItem('userId')
+        value: property?.price,
+        amount: property?.amount,
+        idProperty: property?.id,
+        idUser: localStorage.getItem('userId')
       });
-      setDialogStyle('success')
-      setMsgActions(['Imóvel comprado com sucesso.'])
-      setMsgStart(true)
+      toast('Imóvel comprado com sucesso.', { autoClose: msgTimeout, type: 'success' })
       setTimeout(() => {
-        setMsgActions([])
         history.push('/property/user')
       }, msgTimeout)
     } catch (error) {
-      alert("Algo deu errado, tente novamente.");
+      toast('Algo deu errado, tente novamente.', { autoClose: msgTimeout, type: 'error' })
     }
   }
 
   async function handleHire(e: React.FormEvent<HTMLFormElement>) {
     try {
-      await apiRent.post("/save",{
+      await apiRent.post("/save", {
         data: new Date(),
-        property:property,
-        amountValue:property?.price
+        property: property,
+        amountValue: property?.price
       });
     } catch (error) {
-      alert("Algo deu errado, tente novamente.");
+      toast('Algo deu errado, tente novamente.', { autoClose: msgTimeout, type: 'error' })
     }
   }
 
@@ -148,8 +136,8 @@ function Property() {
             max-w-6xl overflow-y-auto scrollbar-thumb-rounded-full scrollbar-thin
             scrollbar-thumb-red-400 scrollbar-track-transparent"
 
-            >
-            {msgActions[0] && (<TimedDialog dialogStyle={dialogStyle} timeout={msgTimeout} msg={msgActions} start={msgStart} />)}
+          >
+            <ToastContainer />
             <div className="flex items-center justify-around font-qsand w-full">
               <div className="flex flex-col justify-center items-center">
                 <Carousel className="max-w-lg max-h-96" value={carousel} onChange={carouselOnChange} plugins={['arrows']}>
@@ -197,22 +185,22 @@ function Property() {
                       Alugar
                     </button>
                   </div>
-                ):(
+                ) : (
                   <div className="grid place-items-center bg-red-400 h-12 text-lg text-white">
                     <p>Já comprado</p>
                   </div>
                 )}
-                
+
               </div>
             </div>
             <div className="flex max-w-5xl w-full justify-around shadow-md
               rounded-xl bg-white dark:text-white dark:bg-gray-800 p-4 gap-16"
             >
-              <PropertyData name="Localização" value={property?.adress}/>
-              <PropertyData name="Bairro" value={property?.neighborhood}/>
-              <PropertyData name="Número" value={property?.number}/>
-              <PropertyData name="Bloco" value={property?.block}/>
-              <PropertyData name="Quartos" value={property?.rooms}/>
+              <PropertyData name="Localização" value={property?.adress} />
+              <PropertyData name="Bairro" value={property?.neighborhood} />
+              <PropertyData name="Número" value={property?.number} />
+              <PropertyData name="Bloco" value={property?.block} />
+              <PropertyData name="Quartos" value={property?.rooms} />
             </div>
             <div className="flex max-w-3xl w-full shadow-md rounded-xl
               justify-around items-center gap-12 bg-white dark:bg-gray-800
@@ -221,7 +209,7 @@ function Property() {
 
               <div>
                 <ToastContainer />
-                <p className="text-xl text-right mb-5"><span className="text-red-400 font-bold">FALE AGORA</span><br/> COM UM CORRETOR</p>
+                <p className="text-xl text-right mb-5"><span className="text-red-400 font-bold">FALE AGORA</span><br /> COM UM CORRETOR</p>
                 <form
                   onSubmit={handleSubmit}
                   className="flex flex-col gap-3"
@@ -231,7 +219,7 @@ function Property() {
                     <textarea
                       placeholder={`Olá, tenho interesse neste imóvel: ${property?.name}, ${property?.adress}. Gostaria de marcar uma visita presencial, aguardo contato.`}
                       onChange={(e) =>
-                        setData({ ...data, message: e.target.value!='' ? e.target.value : `Olá, tenho interesse neste imóvel: ${property?.name}, ${property?.adress}. Gostaria de marcar uma visita presencial, aguardo contato.`})
+                        setData({ ...data, message: e.target.value != '' ? e.target.value : `Olá, tenho interesse neste imóvel: ${property?.name}, ${property?.adress}. Gostaria de marcar uma visita presencial, aguardo contato.` })
                       }
                       className="focus:ring focus:ring-red-200 px-3 rounded-lg
                       bg-gray-200 dark:bg-gray-600 resize-y max-h-44 h-36"
@@ -287,7 +275,7 @@ function Property() {
                 </form>
               </div>
               <div className="flex flex-1 p-8">
-                <img src={ContactUsSvg} alt="Entre em contato"/>
+                <img src={ContactUsSvg} alt="Entre em contato" />
               </div>
             </div>
           </section>
